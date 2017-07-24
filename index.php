527eba9a347b88f2f90cf7763db8fe907a463716
Rawa Jalal <rawa.jalal@gmail.com>
2017-07-24 16:03:37 -0400

    product template

--- index.php
+++ index.php
@@ -7,7 +7,67 @@ include('includes/header.html');
         </div>
         <hr>
         <div class="row" id="shoppingCartRow">
-            
+            <div class="col-sm-12 col-md-12">
+                <table id="shoppingCart" class="table table-responsive">
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Quantity</th>
+                            <th class="text-center">Price</th>
+                            <th class="text-center">Total</th>
+                            <th> </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>   </td>
+                            <td>   </td>
+                            <td>   </td>
+                            <td>
+                                <h5>Subtotal</h5>
+                            </td>
+                            <td class="text-right">
+                                <h5><strong>$24.59</strong></h5>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>   </td>
+                            <td>   </td>
+                            <td>   </td>
+                            <td>
+                                <h5>Estimated shipping</h5>
+                            </td>
+                            <td class="text-right">
+                                <h5><strong>$6.94</strong></h5>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>   </td>
+                            <td>   </td>
+                            <td>   </td>
+                            <td>
+                                <h3>Total</h3>
+                            </td>
+                            <td class="text-right">
+                                <h3><strong>$31.53</strong></h3>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>   </td>
+                            <td>   </td>
+                            <td>   </td>
+                            <td>
+                                <button type="button" class="btn btn-default">
+                                            <span class="glyphicon glyphicon-shopping-cart"></span> Continue Shopping
+                                        </button></td>
+                            <td>
+                                <button type="button" class="btn btn-success">
+                                            Checkout <span class="glyphicon glyphicon-play"></span>
+                                        </button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 
--- scripts/assignment4.js
+++ scripts/assignment4.js
@@ -1,57 +1,22 @@
-function CreateId(name) {
-    return name.replace(/\s+/g, '-').toLowerCase();
-}
+var ProductList = function () {
+    var productList = new Array();
 
-function Product(name, price, image,description="") {
-    //public properties (this)
-    this.name = name;
-    this.price = price;
-    this.image = image;
-    this.description=description
-    this.id = CreateId(this.name);
-}
 
-function CartItem(product,quantity){
-    this.product = product;
-    this.quantity = quantity;
-}
 
-var ShoppingCart = function(){
-    shoppingCart = new Array();
-    var HasProduct = function(product){
-        for(var i=0;i<shoppingCart.length;i++){
-            if (shoppingCart[i].product.name===product.name){
-                return true;
+    var AddProduct = function (product) {
+        productList.push(product);
+    }
+    var GetProductById = function (id) {
+        for (var i = 0; i < productList.length; i++) {
+            if (productList[i].id == id) {
+                return productList[i];
             }
-        }
-        return false;
-    }
-    var FindProductIndex = function(product){
-        for(var i=0;i<shoppingCart.length;i++){
-            if (shoppingCart[i].product.name===product.name){
-                return i;
-            }
-        }
-    }
-    var AddToCart = function(product){
-        if (!HasProduct(product)){
-            shoppingCart.push(new CartItem(product,1));
-        } else {
-            shoppingCart[FindProductIndex(product)].quantity++;
-        }
-    }
-    var RemoveFromCart = function(product){
-        "use strict";
-        if (HasProduct(product) && shoppingCart[FindProductIndex(product)].quantity===1){
-            shoppingCart.splice(FindProductIndex(product),1);
-        } else if (HasProduct(product) && shoppingCart[FindProductIndex(product)].quantity > 1){
-            shoppingCart[FindProductIndex(product)].quantity--;
         }
     }
     return {
-        shoppingCart:shoppingCart,
-        AddToCart:AddToCart,
-        RemoveFromCart:RemoveFromCart
+        AddProduct: AddProduct,
+        GetProductById: GetProductById,
+        productList: productList
     }
 
 }();
@@ -61,12 +26,83 @@ phone.description = "Apple iPhone 6. Apple iPhone 6 smartphone was launched in S
 var xbox = new Product("Xbox", 299.99, "images/xbox.jpg");
 xbox.description = "Each console has a variety of games. Most games released on the original Xbox are backwards compatible and can be played directly on its successor, Xbox 360. Backward compatibility with Xbox 360 titles was added to Xbox One in June 2015, although titles requiring Kinect or USB peripherals will not be supported.";
 
-var productList = new Array(phone, xbox);
+ProductList.AddProduct(phone);
+ProductList.AddProduct(xbox);
 
-
-
-
-window.onload = function () {
-
-    console.log(ShoppingCart.shoppingCart);
+function CreateId(name) {
+    return name.replace(/\s+/g, '-').toLowerCase();
 }
+
+function Product(name, price, image, description = "") {
+    //public properties (this)
+    this.name = name;
+    this.price = price;
+    this.image = image;
+    this.description = description
+    this.id = CreateId(this.name);
+}
+
+function CartItem(product, quantity) {
+    this.product = product;
+    this.quantity = quantity;
+}
+
+var ShoppingCart = function () {
+    var shoppingCart = new Array();
+
+    //returns -1 if no product is found, otherwise returns the index
+    var FindProductIndex = function (product) {
+        for (var i = 0; i < shoppingCart.length; i++) {
+            if (shoppingCart[i].product.id === product.id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    var FindProductIndexById = function (id) {
+        for (var i = 0; i < shoppingCart.length; i++) {
+            if (shoppingCart[i].product.id == id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    var AddToCart = function (product) {
+        "use strict";
+        if (FindProductIndex(product) === -1) {
+            shoppingCart.push(new CartItem(product, 1));
+            return shoppingCart[FindProductIndex(product)];
+
+        } else {
+            shoppingCart[FindProductIndex(product)].quantity++;
+            return null;
+        }
+    }
+    var RemoveFromCart = function (product) {
+        "use strict";
+        if (FindProductIndex(product) != -1 && shoppingCart[FindProductIndex(product)].quantity === 1) {
+            shoppingCart.splice(FindProductIndex(product), 1);
+        } else if (FindProductIndex(product) != -1 && shoppingCart[FindProductIndex(product)].quantity > 1) {
+            shoppingCart[FindProductIndex(product)].quantity--;
+        }
+    }
+
+    var RemoveAllFromCart = function (product) {
+        "use strict";
+        if (FindProductIndex(product) != -1) {
+            shoppingCart.splice(FindProductIndex(product), 1);
+        }
+    }
+    return {
+        shoppingCart: shoppingCart,
+        AddToCart: AddToCart,
+        RemoveFromCart: RemoveFromCart,
+        RemoveAllFromCart: RemoveAllFromCart,
+        FindProductIndexById: FindProductIndexById
+    }
+
+}();
+
+
+
+window.onload = function () {}
